e0cfaa8ab3fcfb47a026d106fe4157878b310e13
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 16:27:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+ hash[hash]
 end
 
 def update_counting_hash(hash, key)
