ac299f6a11f7cb63ba07a1346a620ebf77015a3b
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 16:18:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,8 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  k = key
+  hash = {k: value}
 end
 
 def read_from_hash(hash, key)
